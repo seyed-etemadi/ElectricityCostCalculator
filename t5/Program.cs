4b07e63f22db43ff31fffdfa6ccb20c4d6dbd18a
seyed-etemadi <seyed.etemadi@gmail.com>
2025-04-06 10:20:30 +0330

    Add project files.

---
+++ t5/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace t5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int consumption, cost;
+            // دریافت میزان مصرف برق از کاربر
+            Console.WriteLine(" Please enter the electricity consumption in kilowatts : ");
+            consumption = int.Parse(Console.ReadLine());
+
+            // محاسبه هزینه برق مصرفی
+            cost = 0;
+
+            if (consumption <= 5)
+            {
+                cost = 0; // Free
+            }
+            else if (consumption > 5 && consumption <= 20)
+            {
+                cost = (consumption - 5) * 1000; // Each kilowatt in excess of 5, 1000 Tomans
+            }
+            else if (consumption > 20)
+            {
+                cost = (15 * 1000) + ((consumption - 20) * 5000); // Cost up to 20 kW and above 20 kW
+            }
+
+            // اضافه کردن آبونمان
+            cost += 10000;
+
+            // نمایش نتیجه
+            Console.WriteLine($" Your electricity cost is: {cost} Tomans.");
+            Console.ReadKey();
+        }
+    }
+}
